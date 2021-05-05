ee3cb3dd57990db1b9f89fe2c033a24ba607852f
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-05-05 16:29:28 +0900

    コメント追加、prettier追加

--- server.rb
+++ server.rb
@@ -8,12 +8,16 @@ WEBrick::HTTPServlet::FileHandler.add_handler("erb", WEBrick::HTTPServlet::ERBHa
 #例えば <% val="test" %> <p><%= val%></p> を <p>test</p>にしてクライアント（ブラウザとか）に返す
 server = WEBrick::HTTPServer.new({ 
 	:DocumentRoot => './documentroot/',
-	:BindAddress => '0.0.0.0', #コレやっておかないとデフォルトのlocalhostになってdockerマシン内からしかアクセス出来ない
+	:BindAddress => '0.0.0.0',
 	:Port => 8000,
 	:MimeTypes => {"erb"=>"text/html"}
 })
 server.start
 
+
 #この記述書いておけばサーバーとして起動してくれるみたい。
 #他にもやり方あるみたいだけど簡単そうだったからこれにしました。
 #とりあえずこの記述入れておくとアクセスしたら./documentroot以下のファイルが返されるみたい
+
+#htmlが返されるとブラウザ側でいろいろ処理してくれるよ。例えば、cssが必要だってhtmlに書いてあったらcssファイルをブラウザがサーバーにアクセスして持ってきて
+#最終的にきれいに描画される感じ。jsファイルも同じ
