bee95916660caee82bfde14871d639f52866b6e6
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-05-06 18:54:53 +0900

    開発環境設定

--- Gemfile
+++ Gemfile
@@ -6,5 +6,6 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
-gem "sqlite3"
-gem "solargraph"
+gem "sqlite3", '1.4.2'
+gem "solargraph", '0.40.4'
+gem "rubocop", '1.14.0'
--- server.rb
+++ server.rb
@@ -1,19 +1,17 @@
 require 'webrick'
 
-
 # ↓この記述いれるとhtmlに埋め込まれた<%= %>とか<% %>のrubyスクリプトを処理してくれるみたい。 コメントアウトするとただhtml返すだけになる
-WEBrick::HTTPServlet::FileHandler.add_handler("erb", WEBrick::HTTPServlet::ERBHandler)
+WEBrick::HTTPServlet::FileHandler.add_handler('erb', WEBrick::HTTPServlet::ERBHandler)
 
 # ./documentrootに置かれたファイルを実行して結果を返す
 # 例えば <% val="test" %> <p><%= val%></p> を <p>test</p>にしてクライアント（ブラウザとか）に返す
-server = WEBrick::HTTPServer.new({ 
-	:DocumentRoot => './documentroot/',
-	:BindAddress => '0.0.0.0',
-	:Port => 8000,
-	:MimeTypes => {"erb"=>"text/html"}
-})
+server = WEBrick::HTTPServer.new({
+                                   DocumentRoot: './documentroot/',
+                                   BindAddress: '0.0.0.0',
+                                   Port: 8000,
+                                   MimeTypes: { 'erb' => 'text/html' }
+                                 })
 server.start
-
 
 # この記述書いておけばサーバーとして起動してくれるみたい。
 # 他にもやり方あるみたいだけど簡単そうだったからこれにしました。
